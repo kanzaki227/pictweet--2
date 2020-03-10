44acb9e227c822fcf052be74fa7ffb47473620af
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-03-10 19:08:46 +0900

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :tweets do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
 
