8abd734e4c12c2122acb897c8443784f6c6ab0d2
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-03-10 21:03:09 +0900

    ツイート検索機能実装
    ツイート検索機能実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,13 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
+
+  # ツイートの検索機能
+  def self.search(search)
+    if search
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
