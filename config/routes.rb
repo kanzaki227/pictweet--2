4f91d71b5be93df5ad28df520c38aded1712a045
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-03-10 16:36:43 +0900

    user名表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
   resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :users, only: :show
 end
+
